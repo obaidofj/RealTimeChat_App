496aa69a5f4628d1b43d9d527eae3f30cec4292b
obaidofj <obaidofj@gmail.com>
2023-11-02 10:47:26 +0200

    agin check if region is the proplem cuase nothing changes in instance, on that region

--- app.ts
+++ app.ts
@@ -34,7 +34,7 @@ import { Server } from 'http';
 import fs from 'fs';
 import { Server as SocketIOServer } from 'socket.io';
 import session, { SessionData } from 'express-session';
-
+ 
 
 
 const __filename = fileURLToPath(import.meta.url);
